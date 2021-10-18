1ac95be76d3bcc8cdcde21d43ef201d1158a1890
amit-doxel <amit.doxel@gmail.com>
2021-10-18 22:45:47 +0530

    added hook for user data (#7)

---
+++ src/hooks/useIdentity.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+export interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  admin: boolean;
+  theme: string;
+  position: string;
+  dismissed_welcome_screen: boolean;
+  phone_number: string;
+  first_time_login: boolean;
+}
+
+export function useIdentity() {
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+  const getCurrentUser = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/v2/users/1', {
+        method: 'GET',
+        credentials: 'include'
+      });
+      const currentUser = await response.json();
+      setUser(currentUser);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return {
+    user,
+  };
+}

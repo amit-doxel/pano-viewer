3c4dd528db267e8839de683cc430979f37ca725e
Amit Chaurasia <amit-doxel@gmail.com>
2021-10-21 00:16:02 +0530

    made the design responsive for mobile view, and passed the zoomIn and zoomOut function as props

--- src/components/PanoViewer/PanoViewer.tsx
+++ src/components/PanoViewer/PanoViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { FloorPlan } from '../FloorPlan';
 import { MiniMap } from '../MiniMap';
@@ -10,12 +10,14 @@ import { Header } from '../Header';
 import { FloorNav } from '../FloorNav';
 import { Arrow } from '../Arrow';
 
-import { useScene, useCountRenders } from '../../hooks';
-import { FloorNavContextProvider } from '../../context/FloorNavContext/FloorNavContextProvider';
+import { zoomInFunction, zoomOutFunction } from '../../utils/zoom';
 
-import { useViewContext } from '../../context/ViewContext/useViewContext';
+import { useScene, useCountRenders } from '../../hooks';
 import { useIdentity } from '../../hooks/useIdentity';
 import { useFetchBlueprint } from '../../hooks/useFetchBlueprint';
+
+import { FloorNavContextProvider } from '../../context/FloorNavContext/FloorNavContextProvider';
+import { useViewContext } from '../../context/ViewContext/useViewContext';
 
 // NOTES:
 // 1. Should have a PanoVisContainer component that can hold
@@ -38,8 +40,7 @@ export const PanoViewer: React.FC = () => {
     view === 'single-pano' ? (
       <>
         <ThreeCanvas scene={scene} camera={camera} />
-        <Arrow url='/assets/icons/left-arrow.svg' style={{ left: '-10px' }} />
-        <Arrow url='/assets/icons/right-arrow.svg' style={{ right: '-10px' }} />
+        <Arrow />
       </>
     ) : (
       <FloorPlan />
@@ -52,7 +53,7 @@ export const PanoViewer: React.FC = () => {
       <LeftBar />
       <MiniMap />
       <FloorNav />
-      <BottomBar />
+      <BottomBar zoomIn={zoomInFunction} zoomOut={zoomOutFunction} />
     </FloorNavContextProvider>
   );
 };
--- src/components/ZoomBar/ZoomBar.tsx
+++ src/components/ZoomBar/ZoomBar.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+import { BottomBarProp } from '../BottomBar';
+import { useScene } from '../../hooks';
 import './styles.css';
 
-export const ZoomBar: React.FC = () => {
+export const ZoomBar: React.FC<BottomBarProp> = ({ zoomIn, zoomOut }) => {
+  const { camera } = useScene();
   return (
     <div className='zoom-wrapper'>
       <div className='bar-theme zoom-bar'>
-        <div className='button' id='zoom-out'>
+        <div className='button' onClick={() => zoomOut(camera)}>
           <img src='assets/icons/minus.svg' alt='zoom-out-icon' />
         </div>
-        <div className='button' id='zoom-in'>
+        <div className='button' onClick={() => zoomIn(camera)}>
           <img src='/assets/icons/add.svg' alt='zoom-in-icon'></img>
         </div>
       </div>
